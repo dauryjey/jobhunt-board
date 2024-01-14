6e9a196d4de8ddfdf4650c8658f96ddd517c5ac6
Daury P <dauryjey@gmail.com>
2024-01-14 02:00:28 -0400

    Fixed brand link

--- app/components/Common/NavigationBar/Brand.tsx
+++ app/components/Common/NavigationBar/Brand.tsx
@@ -3,7 +3,7 @@ import { NavbarBrand } from "flowbite-react";
 
 export const Brand: React.FC = () => {
   return (
-    <NavbarBrand as={Link} to="/">
+    <NavbarBrand as={Link} to="/jobs">
       <span className="text-3xl text-blue-600 font-bold ">#</span>
       <span className="font-bold text-xl ml-2">JobHunt.Dev</span>
     </NavbarBrand>
